7c782181f8c6c4ddb901bbd4069935f80972bb57
ioan beloane <ioanbeloane@gmail.com>
2023-11-27 21:20:42 +0200

    done ch2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index2.css";
-// import App from './App';
-import StarRating from "./StarRating";
-import Challenge from "./Challenge";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
+// import Challenge from "./Challenge";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Challenge />
+    {/* <Challenge /> */}
 
-    {/* <App /> */}
+    <App />
     {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
